e6f8c95dbb45b9a84dedf76b07b8282b3d303be4
OierGago <oier.gagoir@elorrieta-errekamari.com>
2023-10-25 16:04:23 +0200

    Git ignore actualizado

--- app/src/main/java/com/example/appmusicgrupo1/activities/LoginActivity.kt
+++ app/src/main/java/com/example/appmusicgrupo1/activities/LoginActivity.kt
@@ -1,9 +1,10 @@
-package com.example.appmusicgrupo1
+package com.example.appmusicgrupo1.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.appmusicgrupo1.R
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
