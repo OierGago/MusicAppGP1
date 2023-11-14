900c20ce176a9a6a74b39f709043a3bb422f9a22
OierGago <oier.gagoir@elorrieta-errekamari.com>
2023-11-14 17:33:05 +0100

    Pantalla main echa

---
+++ app/src/main/java/com/example/appmusicgrupo1/ui/main/MainViewModel.kt
@@ -0,0 +1,35 @@
+package com.example.appmusicgrupo1.ui.main
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.CreationExtras
+import com.example.appmusicgrupo1.data.AuthenticationResponse
+import com.example.appmusicgrupo1.data.repository.AuthenticationRepository
+import com.example.appmusicgrupo1.data.repository.AuthenticationRequest
+import com.example.appmusicgrupo1.utils.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+
+
+class MainViewModel(
+    private val authenticationRepository: AuthenticationRepository
+) : ViewModel() {
+
+
+    private val _main = MutableLiveData<Resource<AuthenticationResponse>>()
+    val main : LiveData<Resource<AuthenticationResponse>> get() = _main
+
+
+}
+
+class LoginViewModelFactory(
+    private val authenticationRepository: AuthenticationRepository
+): ViewModelProvider.Factory {
+    override fun <T : ViewModel> create(modelClass: Class<T>, extras: CreationExtras): T {
+        return MainViewModel(authenticationRepository) as T
+    }
+}
